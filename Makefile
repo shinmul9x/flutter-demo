a4ec27c8ae097041f19faf7e66b4e0147bad7693
thuydt-omeez <trongthuy8698@gmail.com>
2024-07-12 17:40:33 +0700

    init

---
+++ Makefile
@@ -0,0 +1,3 @@
+clean:
+	fvm flutter clean
+	fvm flutter pub get
---
+++ android/app/src/main/kotlin/com/example/approuter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.approuter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
